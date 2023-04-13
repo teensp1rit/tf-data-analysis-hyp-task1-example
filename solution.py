b210551f706614a3330882ede565abac3bb99887
teensp1rit <55020493+teensp1rit@users.noreply.github.com>
2023-04-13 23:50:33 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,5 +10,6 @@ def solution(x_success: int,
              y_cnt: int) -> bool:
     
     eps = 0.07
-    
-    return False if x_success / x_cnt <= y_success / y_cnt - eps else True
+    F = lambda x: binom.cdf(k = x, n = y_cnt, p = x_success / x_cnt)
+    p = 1 - F(y_success)
+    return False if p > eps else True
